bdd2164fdaf837cf88934979cdce4ae8144b961e
William Welling <wwelling@library.tamu.edu>
2018-11-01 16:33:27 -0500

    (wip) following camunda model api example

--- src/main/java/org/folio/rest/service/ModCamundaService.java
+++ src/main/java/org/folio/rest/service/ModCamundaService.java
@@ -1,11 +1,17 @@
 package org.folio.rest.service;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.BpmnModelElementInstance;
 import org.camunda.bpm.model.bpmn.instance.Definitions;
+import org.camunda.bpm.model.bpmn.instance.EndEvent;
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
+import org.camunda.bpm.model.bpmn.instance.StartEvent;
+import org.camunda.bpm.model.bpmn.instance.UserTask;
 import org.folio.rest.exception.CamundaServiceException;
 import org.folio.rest.model.Workflow;
 import org.folio.rest.model.repo.WorkflowRepo;
@@ -51,6 +57,8 @@ public class ModCamundaService {
 
   @Autowired
   private HttpService httpService;
+
+  private BpmnModelInstance modelInstance = Bpmn.createEmptyModel();
 
   public Workflow deployWorkflow(String tenant, String token, Workflow workflow)
       throws CamundaServiceException, IOException {
@@ -114,11 +122,22 @@ public class ModCamundaService {
   }
 
   private BpmnModelInstance makeBPMNFromWorkflow(Workflow workflow) {
-    BpmnModelInstance modelInstance = Bpmn.createEmptyModel();
-
     Definitions definitions = modelInstance.newInstance(Definitions.class);
     definitions.setTargetNamespace(TARGET_NAMESPACE);
     modelInstance.setDefinitions(definitions);
+
+    // create the process
+    Process process = createElement(definitions, "process-with-one-task", Process.class);
+
+    // create start event, user task and end event
+    StartEvent startEvent = createElement(process, "start", StartEvent.class);
+    UserTask task1 = createElement(process, "task1", UserTask.class);
+    task1.setName("User Task");
+    EndEvent endEvent = createElement(process, "end", EndEvent.class);
+
+    // create the connections between the elements
+    createSequenceFlow(process, startEvent, task1);
+    createSequenceFlow(process, task1, endEvent);
 
     return modelInstance;
   }
@@ -158,6 +177,25 @@ public class ModCamundaService {
     }
     log.info("Headers {}", request.getHeaders());
     return this.httpService.exchange(url, method, request, JsonNode.class);
+  }
+
+  protected <T extends BpmnModelElementInstance> T createElement(BpmnModelElementInstance parentElement, String id,
+      Class<T> elementClass) {
+    T element = modelInstance.newInstance(elementClass);
+    element.setAttributeValue("id", id, true);
+    parentElement.addChildElement(element);
+    return element;
+  }
+
+  public SequenceFlow createSequenceFlow(Process process, FlowNode from, FlowNode to) {
+    String identifier = from.getId() + "-" + to.getId();
+    SequenceFlow sequenceFlow = createElement(process, identifier, SequenceFlow.class);
+    process.addChildElement(sequenceFlow);
+    sequenceFlow.setSource(from);
+    from.getOutgoing().add(sequenceFlow);
+    sequenceFlow.setTarget(to);
+    to.getIncoming().add(sequenceFlow);
+    return sequenceFlow;
   }
 
 }
