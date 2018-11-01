75de91cc104cdda66e9c0159494ebadc2cb5f882
William Welling <wwelling@library.tamu.edu>
2018-11-01 15:23:02 -0500

    better handle event publish exception

--- src/main/java/org/folio/rest/controller/advice/EventControllerAdvice.java
+++ src/main/java/org/folio/rest/controller/advice/EventControllerAdvice.java
@@ -1,11 +1,9 @@
 package org.folio.rest.controller.advice;
 
-import javax.jms.JMSException;
-
+import org.folio.rest.exception.EventPublishException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.jms.JmsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,8 +14,8 @@ public class EventControllerAdvice {
   private static final Logger logger = LoggerFactory.getLogger(EventControllerAdvice.class);
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  @ExceptionHandler(JMSException.class)
-  public String handleJMSException(JmsException exception) {
+  @ExceptionHandler(EventPublishException.class)
+  public String handleEventPublishException(EventPublishException exception) {
     logger.debug(exception.getMessage(), exception);
     return exception.getMessage();
   }
