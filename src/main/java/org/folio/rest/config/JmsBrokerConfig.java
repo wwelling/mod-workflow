dd7ee434d2429436fca216b1583dfc99eff09ccc
William Welling <wwelling@library.tamu.edu>
2018-11-05 14:37:10 -0600

    use workflow commons

--- src/main/java/org/folio/rest/config/JmsBrokerConfig.java
+++ src/main/java/org/folio/rest/config/JmsBrokerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JmsConfig {
+public class JmsBrokerConfig {
 
   @Value("${event.queue.name}")
   private String eventQueueName;
