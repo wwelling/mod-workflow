aae61857fe3c8e9754104a1f9caabc73175adcda
William Welling <wwelling@library.tamu.edu>
2018-11-06 13:11:34 -0600

    remove object mapping, convert and send event

--- src/main/java/org/folio/rest/SpringOkapiModule.java
+++ src/main/java/org/folio/rest/SpringOkapiModule.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.jms.annotation.EnableJms;
 
-@EnableJms
 @SpringBootApplication
 public class SpringOkapiModule extends SpringBootServletInitializer {
 
--- src/main/java/org/folio/rest/jms/EventProducer.java
+++ src/main/java/org/folio/rest/jms/EventProducer.java
@@ -3,7 +3,6 @@ package org.folio.rest.jms;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.annotation.PostConstruct;
 import javax.jms.JMSException;
 import javax.jms.Queue;
 
@@ -14,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class EventProducer {
@@ -29,23 +26,14 @@ public class EventProducer {
   private WorkflowRepo workflowRepo;
 
   @Autowired
-  private ObjectMapper mapper;
-
-  @Autowired
   private Queue queue;
-
-  @PostConstruct
-  private void init() {
-    this.mapper.writerWithDefaultPrettyPrinter();
-  }
 
   public void send(Event event) throws JMSException, IOException {
     workflowRepo.findByStartTriggerId(event.getTriggerId()).forEach(workflow -> {
       event.setProcessDefinitionIds(new ArrayList<String>(workflow.getProcessDefinitionIds()));
     });
-    String message = mapper.writeValueAsString(event);
-    logger.info("Send [{}]: {}", this.queue.getQueueName(), message);
-    this.jmsMessagingTemplate.convertAndSend(this.queue, message);
+    logger.info("Send [{}]: {}, {}, {}", event.getMethod(), event.getPath(), event.getPayload());
+    this.jmsMessagingTemplate.convertAndSend(this.queue, event);
   }
 
 }
