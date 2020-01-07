bd0c8552f19f1c879e418b345e6ef0d2d30363d9
William Welling <wwelling@library.tamu.edu>
2020-01-07 12:45:53 -0600

    remove non null annotations

--- components/src/main/java/org/folio/rest/workflow/components/AbstractFileTask.java
+++ components/src/main/java/org/folio/rest/workflow/components/AbstractFileTask.java
@@ -1,15 +1,12 @@
 package org.folio.rest.workflow.components;
 
 import javax.persistence.Column;
-import javax.validation.constraints.NotNull;
 
 public abstract class AbstractFileTask extends Task {
 
-  @NotNull
   @Column(nullable = false)
   private String path;
 
-  @NotNull
   @Column(nullable = false)
   private String workflow;
 
