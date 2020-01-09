1804511f704de86621f6bf22a039ba59c784a13c
William Welling <wwelling@library.tamu.edu>
2020-01-09 10:11:40 -0600

    add context properties to processor task

--- components/src/main/java/org/folio/rest/workflow/components/ProcessorTask.java
+++ components/src/main/java/org/folio/rest/workflow/components/ProcessorTask.java
@@ -1,19 +1,30 @@
 package org.folio.rest.workflow.components;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Entity
 public class ProcessorTask extends Task {
 
-  @Column(columnDefinition="TEXT")
+  @Column(columnDefinition = "TEXT")
   private String script;
 
+  @Enumerated(EnumType.STRING)
   private TaskScriptType scriptType;
+
+  @ElementCollection
+  private List<String> contextProperties;
 
   public ProcessorTask() {
     super();
     this.setDelegate("streamingProcessDelegate");
+    this.contextProperties = new ArrayList<String>();
   }
 
   public String getScript() {
@@ -30,6 +41,14 @@ public class ProcessorTask extends Task {
 
   public void setScriptType(TaskScriptType scriptType) {
     this.scriptType = scriptType;
+  }
+
+  public List<String> getContextProperties() {
+    return contextProperties;
+  }
+
+  public void setContextProperties(List<String> contextProperties) {
+    this.contextProperties = contextProperties;
   }
 
 }
