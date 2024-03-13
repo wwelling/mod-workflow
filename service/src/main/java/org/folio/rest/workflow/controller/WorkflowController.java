920a06fac26fb88cfb95098b19a9b3989980e4de
Kevin Day <kday@tamu.edu>
2024-03-13 16:02:19 -0500

    MODWRKFLOW-6: Bring in folio-spring-support and create CQL end point for workflows.
    Bring in `folio-spring-support` at version `8.1.0` pulling in dependencies `folio-spring-base` and `folio-spring-cql`.
    Bring in `spring-mododule-core` test dependency called `spring-test`.
    
    Exclude `spring-boot-starter-logging` from `org.springframework.boot` to avoid compile problems introduced by bringing in `folio-spring-support`.
    
    Remove `application-test.yaml` from `pom.xml` and move that file into `service/src/test/resources/application.yaml`.
    The `service/src/main/resources/application-test.yaml` is no longer being read at that location and must be relocated to the above location.
    
    Bring in `OkHttpConfig` with a stub client to prevent compile problems introduced by bringing in `folio-spring-support`.
    This is a stub class and is not used.
    
    Update all repositories to use `JpaCqlRepository` rather than `JpaRepository` as part of bringing in `folio-spring-support`.
    
    Implement `AbstractCqlService` to set the foundation for potentially creating CQL end points for each of the repositories.
    Implement `WorkflowCqlService`, extending `AbstractCqlService`, for performing the workflow search CQL.
    
    Switch to Lombok's `@Slf4j` annotation for logging in `WorkflowController`.
    Implement `/search` end point for `WorkflowController` as `searchWorkflows()` method.
    
    Add Unit Tests.
    There are some work-arounds where the `@Autowired` is not working as expected in unit tests, even if annotations like `@SpringBootTest` are used.
    
    The `application.yaml` for the tests now has hard-coded values for the build info settings.
    Having the settings like `artifact: "@project.artifactId@"` caused a lot of subtle and hard to debug problems during unit testing.
    The `@project.artifactId@` simply never gets expanded during unit testing.
    
    Add the `/workflows/search` end point to the module descriptor.
    
    The `Jenkinsfile` is updated to work with the latest environment.
    The `Dockerbuild` part of the `Jenkinsfile` is not working and so is disabled (commented out).

--- service/src/main/java/org/folio/rest/workflow/controller/WorkflowController.java
+++ service/src/main/java/org/folio/rest/workflow/controller/WorkflowController.java
@@ -1,30 +1,36 @@
 package org.folio.rest.workflow.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import org.folio.rest.workflow.exception.WorkflowEngineServiceException;
 import org.folio.rest.workflow.model.Workflow;
+import org.folio.rest.workflow.service.WorkflowCqlService;
 import org.folio.rest.workflow.service.WorkflowEngineService;
 import org.folio.spring.tenant.annotation.TenantHeader;
 import org.folio.spring.web.annotation.TokenHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping({"/workflows", "/workflows/"})
 public class WorkflowController {
 
-  private static final Logger logger = LoggerFactory.getLogger(WorkflowEngineService.class);
-
   @Autowired
   private WorkflowEngineService workflowEngineService;
+
+  @Autowired
+  private WorkflowCqlService workflowCqlService;
 
   @PutMapping({"/{id}/activate", "/{id}/activate/"})
   public Workflow activateWorkflow(
@@ -32,7 +38,7 @@ public class WorkflowController {
     @TenantHeader String tenant,
     @TokenHeader String token
   ) throws WorkflowEngineServiceException {
-    logger.info("Activating: {}", id);
+    log.info("Activating: {}", id);
     return workflowEngineService.activate(id, tenant, token);
   }
 
@@ -42,7 +48,7 @@ public class WorkflowController {
     @TenantHeader String tenant,
     @TokenHeader String token
   ) throws WorkflowEngineServiceException {
-    logger.info("Deactivating: {}", id);
+    log.info("Deactivating: {}", id);
     return workflowEngineService.deactivate(id, tenant, token);
   }
 
@@ -53,7 +59,7 @@ public class WorkflowController {
     @TokenHeader String token,
     @RequestBody JsonNode context
   ) throws WorkflowEngineServiceException {
-    logger.info("Starting: {} with context {}", id, context);
+    log.info("Starting: {} with context {}", id, context);
     return workflowEngineService.start(id, tenant, token, context);
   }
 
@@ -63,8 +69,19 @@ public class WorkflowController {
     @TenantHeader String tenant,
     @TokenHeader String token
   ) throws WorkflowEngineServiceException {
-    logger.debug("Retrieving History: {}", id);
+    log.debug("Retrieving History: {}", id);
     return workflowEngineService.history(id, tenant, token);
+  }
+
+  @GetMapping(value = "/search", produces = { MediaType.APPLICATION_JSON_VALUE })
+  public JsonNode searchWorkflows(
+    @RequestParam String query,
+    @RequestParam(defaultValue="0") Long offset,
+    @RequestParam(defaultValue="20") Integer limit,
+    @TenantHeader String tenant
+  ) {
+    log.debug("Performing CQL search: {}, offset, limit", query, offset, limit);
+    return workflowCqlService.findByCql(query, offset, limit);
   }
 
 }
---
+++ service/src/main/java/org/folio/rest/workflow/service/WorkflowCqlService.java
@@ -0,0 +1,36 @@
+package org.folio.rest.workflow.service;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.StringUtils;
+import org.folio.rest.workflow.model.Workflow;
+import org.folio.rest.workflow.model.repo.WorkflowRepo;
+import org.folio.spring.data.OffsetRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkflowCqlService extends AbstractCqlService<Workflow> {
+
+  @Autowired
+  private WorkflowRepo repo;
+
+  @Override
+  protected String getTypeName() {
+    return Workflow.class.getSimpleName().toLowerCase() + "s";
+  }
+
+  @Override
+  public ObjectNode findByCql(String query, Long offset, Integer limit) {
+    Page<Workflow> page = null;
+
+    if (StringUtils.isBlank(query)) {
+      page = repo.findAll(new OffsetRequest(offset, limit));
+    } else {
+      page = repo.findByCql(query, new OffsetRequest(offset, limit));
+    }
+
+    return toJson(page.toList(), repo.count());
+  }
+
+}
