b4af7d3117811b2fa2a42bbca84189b3086935b1
William Welling <wwelling@tamu.edu>
2024-10-17 16:48:46 -0500

    Test send to topic workflow.event

--- service/src/main/java/org/folio/rest/workflow/kafka/EventProducer.java
+++ service/src/main/java/org/folio/rest/workflow/kafka/EventProducer.java
@@ -3,6 +3,8 @@ package org.folio.rest.workflow.kafka;
 import org.folio.spring.messaging.model.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,26 +12,17 @@ public class EventProducer {
 
   private static final Logger logger = LoggerFactory.getLogger(EventProducer.class);
 
-  // private JmsMessagingTemplate jmsMessagingTemplate;
+  private KafkaTemplate<String, Event> eventTemplate;
 
-  // private Queue queue;
-
-  // @Autowired
-  // public EventProducer(JmsMessagingTemplate jmsMessagingTemplate, Queue queue) {
-  //   this.jmsMessagingTemplate = jmsMessagingTemplate;
-  //   this.queue = queue;
-  // }
-
-  // public void send(Event event) throws JMSException, IOException {
-  //   // CAUTION: payload not allowed in logs
-  //   logger.info("Send [{}]: {}, {}", queue.getQueueName(), event.getMethod(), event.getPath());
-  //   this.jmsMessagingTemplate.convertAndSend(queue, event);
-  // }
+  @Autowired
+  public EventProducer(KafkaTemplate<String, Event> eventTemplate) {
+    this.eventTemplate = eventTemplate;
+  }
 
   public void send(Event event) {
     // CAUTION: send not implemented
     logger.info("Send []: {}, {}", event.getMethod(), event.getPath());
-
+    this.eventTemplate.send("workflow.events", event);
   }
 
 }
